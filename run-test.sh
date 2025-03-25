f74c601d53e2d52a4eca28bdf971991b7e49f3b3
Pavel Vanecek <corkscreewe@gmail.com>
2025-03-25 22:52:50 +0900

    run_yarn_test_if_exists

--- run-test.sh
+++ run-test.sh
@@ -45,11 +45,27 @@ function yarn_verify_single_dependency {
   fi
 }
 
+function run_yarn_test_if_exists {
+  if [ ! -f "package.json" ]; then
+    exit 2
+  fi
+  # yarn does not support the --if-present option so we have to parse the package.json file
+  local test_script
+  # let's assume that jq is not installed in the environment
+  test_script=$(grep '"test":' package.json || echo "")
+  if [ -n "$test_script" ]; then
+    yarn run test
+  else
+    echo "No test script found in package.json"
+  fi
+}
+
 function npm_test {
   local integration=$1
   pushd "$integration"
   rm -rf node_modules
   npm install --package-lock=false
+  npm run test --if-present
   npm run build
   npm_verify_single_dependency recharts
   npm_verify_single_dependency react
@@ -62,11 +78,12 @@ function npm_test {
 function yarn_test {
   local integration=$1
   pushd "$integration"
-  rm -rf node_modules
-  rm -f yarn.lock
+#  rm -rf node_modules
+#  rm -f yarn.lock
   # so we don't want to generate the lockfile for CI run but yarn refuses to run `yarn list` and `yarn why` without lockfile so we need one anyway
-  yarn install # --frozen-lockfile
-  yarn build
+#  yarn install # --frozen-lockfile
+  run_yarn_test_if_exists
+#  yarn build
   yarn_verify_single_dependency recharts
   yarn_verify_single_dependency react
   yarn_verify_single_dependency 'react-dom'
