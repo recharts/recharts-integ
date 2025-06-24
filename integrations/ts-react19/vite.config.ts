9aa70ae9e7544c6534a2113f6ef380cd4c2e3c83
Pavel Vanecek <corkscreewe@gmail.com>
2025-06-24 23:47:17 +0900

    ignore typescript error in vite config files
    not sure why is this lighting up now but this is not what we wanted to test here

--- integrations/ts-react19/vite.config.ts
+++ integrations/ts-react19/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-expect-error can't figure out how to type this file correctly
   test: {
     environment: 'jsdom'
   }
