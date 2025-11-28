145affdf6833d0e5e4d5ccc6a57a0ee708dad999
Pavel Vanecek <corkscreewe@gmail.com>
2025-11-28 10:38:58 +0900

    Fix npm install missing output

--- test-ui/server/scripts/Controller.ts
+++ test-ui/server/scripts/Controller.ts
@@ -1,59 +1,69 @@
-import { execSync } from 'child_process';
-import { replacePackageVersion } from './replacePackageVersion.ts';
-import { tgzFileNameToPackageJsonReference } from './tgzFileNameToPackageJsonReference.ts';
-import path from 'path';
-import fs from 'fs';
-import { TestOutcome } from './TestOutcome.ts';
+import { execSync } from "child_process";
+import { replacePackageVersion } from "./replacePackageVersion.ts";
+import { tgzFileNameToPackageJsonReference } from "./tgzFileNameToPackageJsonReference.ts";
+import path from "path";
+import fs from "fs";
+import { TestOutcome } from "./TestOutcome.ts";
 
 export abstract class Controller {
-    absolutePath: string;
+  absolutePath: string;
 
-    constructor(absolutePath: string) {
-        this.absolutePath = absolutePath;
-    }
+  constructor(absolutePath: string) {
+    this.absolutePath = absolutePath;
+  }
 
-    clean(): TestOutcome {
-        fs.rmSync(path.join(this.absolutePath, 'node_modules'), { recursive: true, force: true });
-        fs.rmSync(path.join(this.absolutePath, 'package-lock.json'), { force: true });
-        fs.rmSync(path.join(this.absolutePath, 'yarn.lock'), { force: true });
-        const files = fs.readdirSync(this.absolutePath);
-        files.filter(file => file.endsWith('.tgz')).forEach(file => {
-            fs.rmSync(path.join(this.absolutePath, file), { force: true });
-        });
-        return TestOutcome.ok('clean');
-    }
+  clean(): TestOutcome {
+    fs.rmSync(path.join(this.absolutePath, "node_modules"), {
+      recursive: true,
+      force: true,
+    });
+    fs.rmSync(path.join(this.absolutePath, "package-lock.json"), {
+      force: true,
+    });
+    fs.rmSync(path.join(this.absolutePath, "yarn.lock"), { force: true });
+    const files = fs.readdirSync(this.absolutePath);
+    files
+      .filter((file) => file.endsWith(".tgz"))
+      .forEach((file) => {
+        fs.rmSync(path.join(this.absolutePath, file), { force: true });
+      });
+    return TestOutcome.ok("clean");
+  }
 
-    execSync(cmd: string): string {
-        const env = this.getEnv();
-        return execSync(cmd, {
-            cwd: this.absolutePath,
-            encoding: 'utf-8',
-            env,
-            stdio: ['pipe', 'pipe', 'inherit']
-        });
-    }
+  execSync(cmd: string): string {
+    const env = this.getEnv();
+    return execSync(cmd, {
+      cwd: this.absolutePath,
+      encoding: "utf-8",
+      env,
+      stdio: "pipe",
+    });
+  }
 
-    getEnv(): NodeJS.ProcessEnv {
-        return {
-            CI: process.env.CI,
-            PATH: process.env.PATH,
-            COREPACK_ENABLE_AUTO_PIN: '0',
-            COREPACK_ENABLE_STRICT: '0'
-        };
-    }
+  getEnv(): NodeJS.ProcessEnv {
+    return {
+      CI: process.env.CI,
+      PATH: process.env.PATH,
+      COREPACK_ENABLE_AUTO_PIN: "0",
+      COREPACK_ENABLE_STRICT: "0",
+    };
+  }
 
-    replacePackageJsonVersion(dependencyName: string, version: string): TestOutcome {
-        const packageJsonPath = path.join(this.absolutePath, 'package.json');
-        return replacePackageVersion(packageJsonPath, dependencyName, version);
-    }
+  replacePackageJsonVersion(
+    dependencyName: string,
+    version: string,
+  ): TestOutcome {
+    const packageJsonPath = path.join(this.absolutePath, "package.json");
+    return replacePackageVersion(packageJsonPath, dependencyName, version);
+  }
 
-    tgzFileNameToPackageJsonReference(tgzFileName: string): string {
-        return tgzFileNameToPackageJsonReference(this.absolutePath, tgzFileName);
-    }
+  tgzFileNameToPackageJsonReference(tgzFileName: string): string {
+    return tgzFileNameToPackageJsonReference(this.absolutePath, tgzFileName);
+  }
 
-    abstract verifySingleDependencyVersion(dependencyName: string): TestOutcome;
-    abstract install(): TestOutcome;
-    abstract test(): TestOutcome;
-    abstract build(): TestOutcome;
-    abstract pack(): string;
+  abstract verifySingleDependencyVersion(dependencyName: string): TestOutcome;
+  abstract install(): TestOutcome;
+  abstract test(): TestOutcome;
+  abstract build(): TestOutcome;
+  abstract pack(): string;
 }
--- test-ui/server/scripts/NpmController.ts
+++ test-ui/server/scripts/NpmController.ts
@@ -1,67 +1,71 @@
-import { Controller } from './Controller.ts';
-import { TestOutcome } from './TestOutcome.ts';
+import { Controller } from "./Controller.ts";
+import { TestOutcome } from "./TestOutcome.ts";
 
 export class NpmController extends Controller {
-    install(): TestOutcome {
-        try {
-            this.execSync('npm install');
-            return TestOutcome.ok('install');
-        } catch (ex) {
-            return TestOutcome.fail('install', ex as Error);
+  install(): TestOutcome {
+    try {
+      this.execSync("npm install");
+      return TestOutcome.ok("install");
+    } catch (ex: unknown) {
+      return TestOutcome.fail("install", ex);
+    }
+  }
+
+  test(): TestOutcome {
+    try {
+      this.execSync("npm run test --if-present");
+      return TestOutcome.ok("unit test");
+    } catch (ex) {
+      return TestOutcome.fail("unit test", ex as Error);
+    }
+  }
+
+  build(): TestOutcome {
+    try {
+      this.execSync("npm run build");
+      return TestOutcome.ok("build");
+    } catch (ex) {
+      console.error(ex);
+      return TestOutcome.fail("build", ex as Error);
+    }
+  }
+
+  pack(): string {
+    return this.tgzFileNameToPackageJsonReference(
+      this.execSync("npm pack").trim(),
+    );
+  }
+
+  verifySingleDependencyVersion(dependencyName: string): TestOutcome {
+    const versions = this.execSync(`npm ls ${dependencyName} --json`);
+    const parsedVersions = JSON.parse(versions);
+
+    const installedVersions = new Set<string>();
+
+    function walkDependencies(dependencies: any) {
+      for (const [key, value] of Object.entries(dependencies)) {
+        if (key === dependencyName) {
+          installedVersions.add((value as any).version);
         }
+        if ((value as any).dependencies) {
+          walkDependencies((value as any).dependencies);
+        }
+      }
     }
 
-    test(): TestOutcome {
-        try {
-            this.execSync('npm run test --if-present');
-            return TestOutcome.ok('unit test');
-        } catch (ex) {
-            return TestOutcome.fail('unit test', ex as Error);
-        }
+    walkDependencies(parsedVersions.dependencies);
+
+    if (installedVersions.size > 1) {
+      return TestOutcome.fail(
+        dependencyName,
+        new Error(
+          `Multiple versions of ${dependencyName} are installed: ${Array.from(installedVersions).join(", ")}`,
+        ),
+      );
     }
 
-    build(): TestOutcome {
-        try {
-            this.execSync('npm run build');
-            return TestOutcome.ok('build');
-        } catch (ex) {
-            console.error(ex);
-            return TestOutcome.fail('build', ex as Error);
-        }
-    }
-
-    pack(): string {
-        return this.tgzFileNameToPackageJsonReference(this.execSync('npm pack').trim());
-    }
-
-    verifySingleDependencyVersion(dependencyName: string): TestOutcome {
-        const versions = this.execSync(`npm ls ${dependencyName} --json`);
-        const parsedVersions = JSON.parse(versions);
-
-        const installedVersions = new Set<string>();
-
-        function walkDependencies(dependencies: any) {
-            for (const [key, value] of Object.entries(dependencies)) {
-                if (key === dependencyName) {
-                    installedVersions.add((value as any).version);
-                }
-                if ((value as any).dependencies) {
-                    walkDependencies((value as any).dependencies);
-                }
-            }
-        }
-
-        walkDependencies(parsedVersions.dependencies);
-
-        if (installedVersions.size > 1) {
-            return TestOutcome.fail(
-                dependencyName,
-                new Error(`Multiple versions of ${dependencyName} are installed: ${Array.from(installedVersions).join(', ')}`)
-            );
-        }
-
-        const msg = `Dependency ${dependencyName} is installed and only one version is present: ${Array.from(installedVersions).join(', ')}`;
-        console.log(msg);
-        return TestOutcome.ok(msg);
-    }
+    const msg = `Dependency ${dependencyName} is installed and only one version is present: ${Array.from(installedVersions).join(", ")}`;
+    console.log(msg);
+    return TestOutcome.ok(msg);
+  }
 }
