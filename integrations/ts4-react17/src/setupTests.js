7d2854932e497cf75a63883bd44e9b4035d0bdab
Luca Barone <baro.luc@gmail.com>
2026-02-05 19:44:25 +0100

    mock ts4-react17 matchMedia

--- integrations/ts4-react17/src/setupTests.js
+++ integrations/ts4-react17/src/setupTests.js
@@ -1,3 +1,19 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom';
+
+beforeEach(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+})
